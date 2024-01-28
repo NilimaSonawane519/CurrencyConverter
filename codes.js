017a4c40cffcd41555f3f3088652ea6e20f3154c
NilimaSonawane519 <152363473+NilimaSonawane519@users.noreply.github.com>
2024-01-28 17:15:14 +0530

    Add files via upload

---
+++ codes.js
@@ -0,0 +1,19 @@
+const ContryList={
+    AFN:"AF" ,
+    ALL:"AL",
+    AMD:"AM",
+    ANG:"AN",
+    DZD:"DZ",
+     BGN:"BG",
+    EUR:"AT",
+    BBD:"BB",
+   BMD:"BM",
+    AOA:"AO",
+    BRL:"BR",
+    BHD:"BH",
+
+   USD:"US",
+    
+   INR:"IN"
+};
+ 
